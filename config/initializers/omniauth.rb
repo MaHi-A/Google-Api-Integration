01e186dae834a8d1e54e7ef451700c09ea2d1931
Mahesh Anethala <mahesh@eggheadcreative.com>
2015-02-20 17:44:23 +0530

    changed keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '18498877580-is2n0iqd8u2uscspuk3f97c3n8m17ur6.apps.googleusercontent.com',
-           '_VIT4fiKufVkAbbKaa8AN9A_', scope:'userinfo.profile,youtube'
+  provider :google_oauth2, ENV['CLIENT_ID'],ENV['CLIENT_SECRET'], scope:'userinfo.profile,youtube'
 end
