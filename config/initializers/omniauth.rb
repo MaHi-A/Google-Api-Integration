f49070ac19edef863a62b71674f9b18f628dcf19
Mahesh Anethala <mahesh@eggheadcreative.com>
2015-02-20 15:13:25 +0530

    Updated keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, '997400617112-ne35dil79fic3krsh8c1mf0l95o7957o.apps.googleusercontent.com',
-           'i-J_rIy3FB-38ctALfskwx38', scope:'userinfo.profile,youtube'
+           '2H_fHLSNSTGRTl1eyYEvHa11', scope:'userinfo.profile,youtube'
 end
