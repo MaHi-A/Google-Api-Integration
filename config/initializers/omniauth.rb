c70038608aecd49e5b8acc32c81d03cd15b99a4a
Mahesh Anethala <mahesh@eggheadcreative.com>
2015-02-20 17:04:20 +0530

    Minor change

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '997400617112-ne35dil79fic3krsh8c1mf0l95o7957o.apps.googleusercontent.com',
-           '2H_fHLSNSTGRTl1eyYEvHa11', scope:'userinfo.profile,youtube'
+  provider :google_oauth2, '18498877580-is2n0iqd8u2uscspuk3f97c3n8m17ur6.apps.googleusercontent.com',
+           '_VIT4fiKufVkAbbKaa8AN9A_', scope:'userinfo.profile,youtube'
 end
