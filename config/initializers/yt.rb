94a70ee3d64473cd8c0cbc0a045492ffcfa57d71
The Egghead Creative <ehc@ganga.(none)>
2015-02-20 12:03:41 +0530

    Initialized project

---
+++ config/initializers/yt.rb
@@ -0,0 +1,3 @@
+Yt.configure do |config|
+  config.api_key = 'AIzaSyDBgvid96coNufO3gxxQgb-U22pmttWPtw'
+end
---
+++ db/migrate/20150220053620_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :token
+      t.string :uid
+
+      t.timestamps
+    end
+    add_index :users, :uid, unique: true
+  end
+end
