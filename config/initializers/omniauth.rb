399701a8784a35d7edcd69180ae901136f05918c
Mahesh Anethala <mahesh@eggheadcreative.com>
2015-02-23 15:46:23 +0530

    Added auth keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_APP_SECRET'], scope: 'userinfo.profile,youtube'
+  provider :google_oauth2, GOOGLE_APP_ID='262132414086-o44tg1keec2vla2r9lrkaicdh7vft59m.apps.googleusercontent.com',
+           GOOGLE_APP_SECRET='0i_Lp2OV603-6lJsIxQRm2et', scope: 'userinfo.profile,youtube'
 end
 
