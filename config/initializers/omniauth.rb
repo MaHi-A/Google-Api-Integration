361ba3a7ceb80524bb69f4328c4e90c8d7300a50
Mahesh Anethala <mahesh@eggheadcreative.com>
2015-02-23 17:14:13 +0530

    minor refactoring

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, GOOGLE_APP_ID='262132414086-o44tg1keec2vla2r9lrkaicdh7vft59m.apps.googleusercontent.com',
-           GOOGLE_APP_SECRET='0i_Lp2OV603-6lJsIxQRm2et', scope: 'userinfo.profile,youtube'
+           GOOGLE_APP_SECRET='0i_Lp2OV603-6lJsIxQRm2et'
 end
 
